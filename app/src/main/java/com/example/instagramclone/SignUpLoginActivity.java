93fe1a1811f322b4292489031f141e982b7f265d
anvar <ishbayev16@itu.edu.tr>
2019-08-11 18:12:41 +0300

    Logout

--- app/src/main/java/com/example/instagramclone/SignUpLoginActivity.java
+++ app/src/main/java/com/example/instagramclone/SignUpLoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.instagramclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -49,6 +50,10 @@ public class SignUpLoginActivity extends AppCompatActivity{
                             FancyToast.makeText(SignUpLoginActivity.this,
                                     appUser.get("username")+" is signed up seccesfully", FancyToast.LENGTH_LONG,
                                     FancyToast.SUCCESS, true).show();
+
+                            Intent intent = new Intent(SignUpLoginActivity.this, WelcomeActivity.class);
+                            startActivity(intent);
+
                         }else{
                             FancyToast.makeText(SignUpLoginActivity.this, e.getMessage(),
                                     FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
@@ -70,6 +75,10 @@ public class SignUpLoginActivity extends AppCompatActivity{
                                     FancyToast.makeText(SignUpLoginActivity.this,
                                             user.get("username")+" is logged in seccesfully", FancyToast.LENGTH_LONG,
                                             FancyToast.SUCCESS, true).show();
+
+                                    Intent intent = new Intent(SignUpLoginActivity.this, WelcomeActivity.class);
+                                    startActivity(intent);
+
                                 }else{
                                     FancyToast.makeText(SignUpLoginActivity.this, e.getMessage(),
                                             FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
